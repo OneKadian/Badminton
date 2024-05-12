66d281e65fee0d3cecdb74c30e180400f4ee6097
unknown <coderanirudh@gmail.com>
2024-05-12 16:03:38 +0530

    fixed modal alignment

--- app/matches/page.js
+++ app/matches/page.js
@@ -7,7 +7,6 @@ import { FaPlus } from "react-icons/fa6";
 import { RxCross1 } from "react-icons/rx";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-import MatchModal from "../components/CreateMatchModal";
 
 export default function LeaderBoard() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -242,12 +241,12 @@ export default function LeaderBoard() {
                 aria-modal="true"
                 aria-labelledby="modal-headline"
               >
-                <div className="bg-white w-[400px] flex justify-center pt-5 pb-4 ">
-                  <div className="w-[300px] ">
-                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                <div className="bg-white w-[380px] flex justify-center pt-5 pb-4 ">
+                  <div className="w-[300px]">
+                    <div className="md:hidden mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                       <FaRegEye className="h-6 w-6 text-green-600" />
                     </div>
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4">
                       <h3
                         className="text-lg leading-6 font-medium text-gray-900"
                         id="modal-headline"
@@ -327,11 +326,11 @@ export default function LeaderBoard() {
                     </div>
                   </div>
                 </div>
+                {/* Buttons here */}
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
-                    onClick={toggleCreateMatchModal}
                     type="button"
-                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="w-full mt-3 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
                   >
                     Cancel
                   </button>
